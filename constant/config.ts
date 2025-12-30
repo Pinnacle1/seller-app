c33abb266ec8f595cdcc48dfe3d48cd6d4047853
rthangaraj-improva <rthangaraj@improva.io>
2025-12-30 08:31:14 +0530

    feat: Add API URL configuration constants

--- constant/config.ts
+++ constant/config.ts
@@ -1,8 +1,8 @@
 export const Config = {
 
     URL: {
-        API_URL: "http://localhost:8000/",
-        API_V2: "http://localhost:8000/",
+        API_URL: "https://thriftzy-backend.onrender.com/",
+        API_V2: "https://thriftzy-backend.onrender.com/",
     },
 
 }
